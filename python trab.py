87a85832bc48c7050a069ab51c64696d12a348bd
caio <cmarciosssilva@hotmail.com>
2021-12-02 20:09:21 -0300

    função universal para conversão base 10 para 2 e funções de soma e subt

--- python trab.py
+++ python trab.py
@@ -1,44 +1,80 @@
 def espaco():
     return print()
     
-def converte(listaNumsConv, listaNumsConv1):
+def converte(listaConverter):
     potencia = 31
     valorDec = 0
-    valorDec1 = 0
-    
-    
-    if listaNumsConv[0] == 1:
-        listaNumsConv[0] = 0
+    if listaConverter[0] == 1:
+        listaConverter[0] = 0
     
         for j in range(32):
-            valorDec = valorDec + (listaNumsConv[j] * (2 ** potencia))
+            valorDec = valorDec + (listaConverter[j] * (2 ** potencia))
             potencia = potencia - 1
         valorDec *= -1
     
-    elif listaNumsConv[0] == 0:
+    elif listaConverter[0] == 0:
         for j in range(32):
-            valorDec = valorDec + (listaNumsConv[j] * (2 ** potencia))
+            valorDec = valorDec + (listaConverter[j] * (2 ** potencia))
             potencia = potencia - 1
-    print(valorDec)
+    return valorDec
 
-    potencia = 31
-    
-    if listaNumsConv1[0] == 1:
-    
-        listaNumsConv1[0] = 0
-        for j in range(32):
-            valorDec1 = valorDec1 + (listaNumsConv1[j] * (2 ** potencia))
-            potencia = potencia - 1
-        valorDec1 *= -1
-    
-    elif listaNumsConv1[0] == 0:
-    
-        for j in range(32):
-            valorDec1 = valorDec1 + (listaNumsConv1[j] * (2 ** potencia))
-            potencia = potencia - 1
-        
-    print(valorDec1)
-    return listaNumsConv1, listaNumsConv
+
+def soma(listaNumsRev, listaNums1Rev):
+    sobra = 0
+    listaNumsSomadosRev = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0]
+    for i in range(len(listaNumsSomadosRev)):
+
+        if listaNumsRev[i] == 0 and listaNums1Rev[i] == 0:
+
+            if sobra == 0:
+                listaNumsSomadosRev[i] = 0
+            else:
+                listaNumsSomadosRev[i] = 1
+                sobra = 0
+
+        elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 1:
+
+            if sobra == 0:
+                listaNumsSomadosRev[i] = 0
+                sobra = 1
+            else:
+                listaNumsSomadosRev[i] = 1
+                sobra = 1
+        elif listaNumsRev[i] == 0 and listaNums1Rev[i] == 1:
+
+            if sobra == 0:
+                listaNumsSomadosRev[i] = 1
+            else:
+                listaNumsSomadosRev[i] = 0
+                sobra = 1
+        elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 0:
+
+            if sobra == 0:
+                listaNumsSomadosRev[i] = 1
+            else:
+                listaNumsSomadosRev[i] = 0
+                sobra = 1
+    listaSoma = list(reversed(listaNumsSomadosRev))
+    return(listaSoma)
+
+
+
+def subtracao(listaNumsRev, listaNums1Rev):
+    listaNumsSubtraidosRev = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0]
+    for i in range(len(listaNumsSubtraidosRev)):
+        if listaNumsRev[i] == 0 and listaNums1Rev[i] == 0:
+            listaNumsSubtraidosRev[i] = 0
+
+        elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 1:
+            listaNumsSubtraidosRev[i] = 0
+
+        elif listaNumsRev[i] == 0 and listaNums1Rev[i] == 1:
+            listaNumsSubtraidosRev[i] = 1
+
+        elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 0:
+            listaNumsSubtraidosRev[i] = 1
+    listaSubtracao = list(reversed(listaNumsSubtraidosRev))
+    return(listaSubtracao)
 
 
 arquivo = open('entrada.txt', 'r')
@@ -46,20 +82,10 @@ numBin = arquivo.readline().strip('\n')
 numBin1 = arquivo.readline().strip('\n')
 arquivo.close()
 
-
-sobra = 0
-soma = 0
-subtracao = 0
-listaNumsSomadosRev = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-listaNumsSubtraidosRev = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-valorDec = 0
-potencia = 31
 listaNums = []
 listaNumsConv = []
 listaNumsConv1 = []
 listaNums1 = []
-subtConv = 0
-
 
 for i in numBin:
     numInt = int(i)
@@ -71,69 +97,22 @@ for i in numBin1:
     listaNums1.append(numInt1)
     listaNumsConv1.append(numInt1)
 
-converte(listaNumsConv, listaNumsConv1)
+listaContrario = list(reversed(listaNums))
+listaContrario1 = list(reversed(listaNums1))
 
-
-listaNumsRev = list(reversed(listaNums))
-listaNums1Rev = list(reversed(listaNums1))
-
-
-for i in range(len(listaNumsSomadosRev)):
-    
-    if listaNumsRev[i] == 0 and listaNums1Rev[i] == 0:
-        
-        if sobra == 0:
-            listaNumsSomadosRev[i] = 0
-        else:
-            listaNumsSomadosRev[i] = 1
-            sobra = 0
-    
-    elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 1:
-        
-        if sobra == 0:
-            listaNumsSomadosRev[i] = 0
-            sobra = 1
-        else:
-            listaNumsSomadosRev[i] = 1
-            sobra = 1
-    elif listaNumsRev[i] == 0 and listaNums1Rev[i] == 1:
-        
-        if sobra == 0:
-            listaNumsSomadosRev[i] = 1
-        else:
-            listaNumsSomadosRev[i] = 0
-            sobra = 1
-    elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 0:
-        
-        if sobra == 0:
-            listaNumsSomadosRev[i] = 1
-        else:
-            listaNumsSomadosRev[i] = 0
-            sobra = 1
-            
-
-for i in range(len(listaNumsSomadosRev)):
-    
-    if listaNumsRev[i] == 0 and listaNums1Rev[i] == 0:
-        listaNumsSubtraidosRev[i] = 0
-    
-    elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 1:
-        listaNumsSubtraidosRev[i] = 0
-    
-    elif listaNumsRev[i] == 0 and listaNums1Rev[i] == 1:
-        listaNumsSubtraidosRev[i] = 1
-    
-    elif listaNumsRev[i] == 1 and listaNums1Rev[i] == 0:
-        listaNumsSubtraidosRev[i] = 1
-        
-        
-listaNumsSomados = list(reversed(listaNumsSomadosRev))        
-listaNumsSubtraidos = list(reversed(listaNumsSubtraidosRev))
-
+listaNumsSomados = soma(listaContrario, listaContrario1)
+listaNumsSubtraidos = subtracao(listaContrario, listaContrario1)
+conversaoNum = converte(listaNumsConv)
+conversaoNum1 = converte(listaNumsConv1)
+conversaoSoma = converte(listaNumsSomados)
+conversaoSubt = converte(listaNumsSubtraidos)
+print(conversaoNum)
+print(conversaoNum1)
 espaco()
 print(*listaNumsSubtraidos, sep = "")
 print(*listaNumsSomados, sep = "")
 espaco()
+print(conversaoSoma)
+print(conversaoSubt)
 
-converte(listaNumsSubtraidos, listaNumsSomados)
 
